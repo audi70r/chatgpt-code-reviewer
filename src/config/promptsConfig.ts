20150f0405f20ed47f833e25441ae683b0f429c5
dmitribeltjukov <dmitri.beltjukov@gmail.com>
2024-11-05 11:18:22 +0200

    prompt edit

--- src/config/promptsConfig.ts
+++ src/config/promptsConfig.ts
@@ -4,7 +4,7 @@ enum Prompt {
 
 const promptsConfig: { [key in Prompt]: string } = {
   [Prompt.SYSTEM_PROMPT]:
-    'You now assume the role of a code reviewer. Based on the patch provide a list of suggestions how to improve the code with examples according to coding standards and best practices.\nStart every suggestion with path to the file. Path to the file should start with @@ and end with @@',
+    'You now assume the role of a code reviewer. Based on the patch provide a list of suggestions how to improve the code with examples, focusing on identifying potential issues rather than style. Look for any unreachable code, readability challenges, potential bugs, or gaps in error handling.\nStart every suggestion with path to the file. Path to the file should start with @@ and end with @@',
 };
 
 export default promptsConfig;
